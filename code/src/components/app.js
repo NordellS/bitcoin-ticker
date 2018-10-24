2156ed291721cfe2c084f5c5fcf80f4b3b8fc363
SaraNordell <sara@nordell.me>
2018-10-24 23:15:09 +0200

    responsive bitcoin chart using "recharts"

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { LineChart, Line, Tooltip, YAxis, XAxis, CartesianGrid, Legend, ResponsiveContainer } from "recharts"
 import openGdaxWebsocket from "../gdax-websocket"
 
 class App extends React.Component {
@@ -23,12 +24,31 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.tickerMessages.map(msg => (
-          <div key={msg.sequence}>
-            {msg.time}: <strong>{msg.price} EUR</strong>
-          </div>
-        ))}
+      <div id="bitcoinChart">
+        <ResponsiveContainer width="100%" height={500} >
+          <LineChart
+            data={this.state.tickerMessages}>
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="price"
+              name="Price"
+              stroke="#7360FB"
+              animationEasing="ease-in"
+              activeDot={{ r: 7 }} />
+            <CartesianGrid
+              stroke="#ACC3A6"
+              strokeDasharray="3 3" />
+            <YAxis
+              type="number"
+              domain={["dataMin - 0.5", "dataMax + 0.5"]}
+              allowDecimals={false} />
+            <XAxis
+              dataKey="time"
+              tickFormatter="time" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     )
   }
